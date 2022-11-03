23d92b58b07ea578f9ee962ceb54b014f7a669d4
edgar moreno <emocer2@gmail.com>
2022-11-03 13:07:21 +0100

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { contadorComponent } from './contador/contador.component';
+
+
+
+@NgModule({
+  declarations: [
+    contadorComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    contadorComponent
+  ]
+})
+export class ContadorModule { }
